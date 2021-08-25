e0a1d11c85797d4cc5764465a17773e9efd60644
Fabrício Caldana <61567139+NcryScooby@users.noreply.github.com>
2021-08-25 16:08:08 -0300

    Add files via upload

---
+++ exe1.ts
@@ -0,0 +1,11 @@
+let inicio: number = 0;
+let fim: number = 10;
+
+for (inicio; inicio < fim; inicio++) {
+
+
+  while (inicio % 2 == 0) {
+    console.log("numeros pares" + inicio)
+    break
+  }
+}
